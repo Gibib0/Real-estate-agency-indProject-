fcc741f85c8283b8724e83408bad427ca2000b14
Anastasia <anast111antonova@gmail.com>
2025-10-29 19:50:04 +0200

    Add ClientService, PropertyService

--- RealEstateAgency/Services/ClientService.cs
+++ RealEstateAgency/Services/ClientService.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿using RealEstateAgency.Models;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RealEstateAgency.Services
 {
-    internal class ClientService
+    public class ClientService
     {
+        private List<Client> _clients;
+        private JSONDataService _jsonDataService;
+        private string _filePath = @"Data/clientsData.json";
+
+        public ClientService()
+        {
+            _jsonDataService = new JSONDataService();
+            _clients = _jsonDataService.LoadFromFile<Client>(_filePath);
+        }
+        public void AddClient(Client client)
+        {
+            if (!_clients.Any(c => c.FullName.Equals(client.FullName, StringComparison.OrdinalIgnoreCase)))
+            {
+                _clients.Add(client);
+                _jsonDataService.SaveToFile(_clients, _filePath);
+            }
+        }
+
+        public List<Client> GetClients()
+        {
+            return _clients;
+        }
     }
 }
--- RealEstateAgency/Services/PropertyService.cs
+++ RealEstateAgency/Services/PropertyService.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿using RealEstateAgency.Models;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RealEstateAgency.Services
 {
-    internal class PropertyService
+    public class PropertyService
     {
+        private List<Property> _properties;
+        private JSONDataService _jsonDataService;
+        private string _filePath = @"Data/propertiesData.json";
+
+        public PropertyService()
+        {
+            _jsonDataService = new JSONDataService();
+            _properties = _jsonDataService.LoadFromFile<Property>(_filePath);
+        }
+        public void AddProperty(Property property)
+        {
+            if (!_properties.Any(p => p.Address.Equals(property.Address, StringComparison.OrdinalIgnoreCase)))
+            {
+                _properties.Add(property);
+                _jsonDataService.SaveToFile(_properties, _filePath);
+            }
+        }
+
+        public List<Property> GetClients()
+        {
+            return _properties;
+        }
     }
 }
