621bde55a829fbe521552bdd180a7f2a56e81cd6
a-25600 <aleksagunya56@gmail.com>
2025-11-06 22:25:34 +0200

    Add stats functions and changed Landmark into list

---
+++ LandmarkInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RealEstateAgency.Models
+{
+    public class LandmarkInfo
+    {
+        public string Name { get; set; }
+
+        public int TravelTimeMinutes { get; set; }
+
+        public LandmarkInfo() { }
+
+        public LandmarkInfo(string name, int travelTimeMinutes)
+        {
+            Name = name;
+            TravelTimeMinutes = travelTimeMinutes;
+        }
+    }
+}
