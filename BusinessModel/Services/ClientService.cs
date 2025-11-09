db5a201e4037786f96f92635cc0f8087b16de3b6
a-25600 <aleksagunya56@gmail.com>
2025-11-09 21:22:48 +0200

    Create new structure

---
+++ BusinessModel/Services/ClientService.cs
@@ -0,0 +1,68 @@
+﻿using BusinessLogic.Models;
+using Persistence;
+using System.Collections.Generic;
+
+namespace BusinessLogic.Services
+{
+    public class ClientService
+    {
+        private List<Client> _clients;
+        private JSONDataService _jsonDataService;
+        private string _filePath = @"Data/clientsData.json";
+
+        public ClientService()
+        {
+            _jsonDataService = new JSONDataService();
+            _clients = _jsonDataService.LoadFromFile<Client>(_filePath);
+        }
+        public void AddClient(Client client)
+        {
+            if (!_clients.Any(c =>
+                c.Id == client.Id ||
+                c.FullName.Equals(client.FullName, StringComparison.OrdinalIgnoreCase) ||
+                !string.IsNullOrWhiteSpace(c.Email) && c.Email.Equals(client.Email, StringComparison.OrdinalIgnoreCase) ||
+                !string.IsNullOrWhiteSpace(c.Phone) && c.Phone.Equals(client.Phone)))
+            {
+                _clients.Add(client);
+                _jsonDataService.SaveToFile(_clients, _filePath);
+            }
+        }
+
+        public List<Client> GetClients()
+        {
+            return _clients;
+        }
+
+        public List<Client> GetClientByType(Client.ClientType type)
+        {
+            return _clients.Where(c => c.CurrentType == type).ToList();
+        }
+
+        public bool UpdateClient(Client updatedClient)
+        {
+            int index = _clients.FindIndex(p => p.Id == updatedClient.Id);
+
+            if (index != -1)
+            {
+                _clients[index] = updatedClient;
+                _jsonDataService.SaveToFile(_clients, _filePath);
+                return true;
+            }
+
+            return false;
+        }
+        public bool DeleteClient(Guid id)
+        {
+            var clientToRemove = _clients.FirstOrDefault(p => p.Id == id);
+
+            if (clientToRemove != null)
+            {
+                _clients.Remove(clientToRemove);
+                _jsonDataService.SaveToFile(_clients, _filePath);
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
