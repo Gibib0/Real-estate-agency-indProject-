225ea56ea6da6f40de654360ed15777e95e09003
a-25600 <aleksagunya56@gmail.com>
2025-11-13 20:14:58 +0200

    Add server integration

--- Integration/Models/LoginResponse.cs
+++ Integration/Models/LoginResponse.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace Integration.Models
 {
-    public class LoginRequest
+    public class LoginResponse
     {
-        public string Username { get; set; }
-        public string Password { get; set; }
+        public string Token { get; set; }
+        public string Role { get; set; }
     }
 }
