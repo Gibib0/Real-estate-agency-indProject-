3bfe36b816622d6df031b0903ee26791678b92e1
a-25600 <aleksagunya56@gmail.com>
2025-11-14 23:59:54 +0200

    Add database

---
+++ Integration/Migrations/20251114211109_InitialRealEstateSchema.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Integration.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialRealEstateSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Agents",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    FullName = table.Column<string>(type: "TEXT", nullable: false),
+                    Experience = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Agents", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Clients",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    FullName = table.Column<string>(type: "TEXT", nullable: false),
+                    Email = table.Column<string>(type: "TEXT", nullable: false),
+                    Phone = table.Column<string>(type: "TEXT", nullable: false),
+                    CurrentType = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clients", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Properties",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    PropertyType = table.Column<string>(type: "TEXT", nullable: false),
+                    Square = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Address = table.Column<string>(type: "TEXT", nullable: false),
+                    Price = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Rooms = table.Column<int>(type: "INTEGER", nullable: false),
+                    District = table.Column<string>(type: "TEXT", nullable: false),
+                    CurrentStatus = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Properties", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SavedSearches",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    ClientId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    DateSaved = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: false),
+                    Filter_PropertyType = table.Column<string>(type: "TEXT", nullable: false),
+                    Filter_District = table.Column<string>(type: "TEXT", nullable: false),
+                    Filter_Landmark = table.Column<string>(type: "TEXT", nullable: false),
+                    Filter_MaxTravelTime = table.Column<int>(type: "INTEGER", nullable: true),
+                    Filter_MinPrice = table.Column<decimal>(type: "TEXT", nullable: true),
+                    Filter_MaxPrice = table.Column<decimal>(type: "TEXT", nullable: true),
+                    Filter_MinArea = table.Column<decimal>(type: "TEXT", nullable: true),
+                    Filter_MaxArea = table.Column<decimal>(type: "TEXT", nullable: true),
+                    Filter_MinRooms = table.Column<int>(type: "INTEGER", nullable: true),
+                    Filter_MaxRooms = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SavedSearches", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Deals",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Date = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    PropertyId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    AgentId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    ClientId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    FinalPrice = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Type = table.Column<int>(type: "INTEGER", nullable: false),
+                    CommissionPercent = table.Column<decimal>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Deals", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Deals_Agents_AgentId",
+                        column: x => x.AgentId,
+                        principalTable: "Agents",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Deals_Clients_ClientId",
+                        column: x => x.ClientId,
+                        principalTable: "Clients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Deals_Properties_PropertyId",
+                        column: x => x.PropertyId,
+                        principalTable: "Properties",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Landmarks",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    TravelTimeMinutes = table.Column<int>(type: "INTEGER", nullable: false),
+                    PropertyId = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Landmarks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Landmarks_Properties_PropertyId",
+                        column: x => x.PropertyId,
+                        principalTable: "Properties",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StatusChanges",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    OldStatus = table.Column<int>(type: "INTEGER", nullable: false),
+                    NewStatus = table.Column<int>(type: "INTEGER", nullable: false),
+                    Time = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    ChangeBy = table.Column<string>(type: "TEXT", nullable: true),
+                    PropertyId = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StatusChanges", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_StatusChanges_Properties_PropertyId",
+                        column: x => x.PropertyId,
+                        principalTable: "Properties",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Deals_AgentId",
+                table: "Deals",
+                column: "AgentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Deals_ClientId",
+                table: "Deals",
+                column: "ClientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Deals_PropertyId",
+                table: "Deals",
+                column: "PropertyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Landmarks_PropertyId",
+                table: "Landmarks",
+                column: "PropertyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StatusChanges_PropertyId",
+                table: "StatusChanges",
+                column: "PropertyId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Deals");
+
+            migrationBuilder.DropTable(
+                name: "Landmarks");
+
+            migrationBuilder.DropTable(
+                name: "SavedSearches");
+
+            migrationBuilder.DropTable(
+                name: "StatusChanges");
+
+            migrationBuilder.DropTable(
+                name: "Agents");
+
+            migrationBuilder.DropTable(
+                name: "Clients");
+
+            migrationBuilder.DropTable(
+                name: "Properties");
+        }
+    }
+}
